27b24e177b79aafc6e76815356cbf15e87728066
Astral <tan_yu_xuan_astral@s2024.ssts.edu.sg>
2024-09-09 16:02:36 +0800

    Initial Commit

---
+++ lesson 8 again/lesson_8_againApp.swift
@@ -0,0 +1,17 @@
+//
+//  lesson_8_againApp.swift
+//  lesson 8 again
+//
+//  Created by Astral Tan on 9/9/24.
+//
+
+import SwiftUI
+
+@main
+struct lesson_8_againApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
